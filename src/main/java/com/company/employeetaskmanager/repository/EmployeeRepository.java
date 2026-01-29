cc61341d0039caee90b4969ea279be35d8a73af3
Raul Munoz <munozzraul30@gmail.com>
2026-01-29 17:49:19 +0100

    Update JPA repositories for Employee and Task entities

--- src/main/java/com/company/employeetaskmanager/repository/EmployeeRepository.java
+++ src/main/java/com/company/employeetaskmanager/repository/EmployeeRepository.java
@@ -2,15 +2,13 @@ package com.company.employeetaskmanager.repository;
 
 import com.company.employeetaskmanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository
+        extends JpaRepository<Employee, Long> {
 
-    List<Employee> findAllByOrderByNameAsc();
+    List<Employee> findByName(String name);
 
-    List<Employee> findByDepartment(String department);
-
-    Employee findByEmail(String email);
+    List<Employee> findAllByOrderByName();
 }
